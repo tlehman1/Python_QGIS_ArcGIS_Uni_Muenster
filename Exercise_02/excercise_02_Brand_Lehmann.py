2889e34386cfd2ce72e6e8841813468a30627e6b
tlehman1 <102968476+tlehman1@users.noreply.github.com>
2025-04-15 08:33:19 +0200

    eingerückt

--- Exercise_02/excercise_02_Brand_Lehmann.py
+++ Exercise_02/excercise_02_Brand_Lehmann.py
@@ -8,7 +8,7 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
 # +++ your code here +++
-return
+    return
 
 
 # verbing
@@ -20,7 +20,7 @@ return
 # Return the resulting string.
 def verbing(s):
 # +++your code here+++
-return
+    return
 
 
 # Remove adjacent
@@ -30,23 +30,23 @@ return
 # modify the passed in list.
 def remove_adjacent(nums):
 # +++your code here+++
-return
+    return
 
 
 def main():
-print('donuts')
-print(donuts(4))
-print(donuts(9))
-print(donuts(10))
-print(donuts('twentyone'))
-print('verbing')
-print(verbing('hail'))
-print(verbing('swiming'))
-print(verbing('do'))
-print('remove_adjacent')
-print(remove_adjacent([1, 2, 2, 3]))
-print(remove_adjacent([2, 2, 3, 3, 3]))
-print(remove_adjacent([]))
+    print('donuts')
+    print(donuts(4))
+    print(donuts(9))
+    print(donuts(10))
+    print(donuts('twentyone'))
+    print('verbing')
+    print(verbing('hail'))
+    print(verbing('swiming'))
+    print(verbing('do'))
+    print('remove_adjacent')
+    print(remove_adjacent([1, 2, 2, 3]))
+    print(remove_adjacent([2, 2, 3, 3, 3]))
+    print(remove_adjacent([]))
 # Standard boilerplate to call the main() function.
 if __name__ == '__main__':
-main()
+    main()
