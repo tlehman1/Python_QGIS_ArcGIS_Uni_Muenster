38020c57673e8db2051b456987d690e91bc55849
TobiasBrand-GI <tobias.brand1@web.de>
2025-05-07 16:34:16 +0200

    Exercise 4.3 Solution

--- Exercise_04/exercise_4_3.py
+++ Exercise_04/exercise_4_3.py
@@ -4,9 +4,6 @@ from qgis.core import *
 
 import os
 
-# Supply path to qgis install location
-#QgsApplication.setPrefixPath("/path/to/qgis/installation", True)
-
 # Path to data and QGIS-project
 project_path = "Exercise_04\myFirstProject.qgz"  # for QGIS version 3+
 
@@ -15,16 +12,20 @@ project_path = "Exercise_04\myFirstProject.qgz"  # for QGIS version 3+
 path = "Exercise_04\muenster"
 dirs = os.listdir( path )
 
+# initiate QGIS project
 project = QgsProject.instance()
 project.read(project_path)
 
 # Print all the files and directories
 for file in dirs:
+    # filter only shapes
     if file.endswith(".shp"):
-        layer = QgsVectorLayer(path + file, file[:-4], "ogr")
+        #create new layer
+        layer = QgsVectorLayer(f"{path}/{file}", os.path.basename(file), "ogr")
         if not layer.isValid():
             print("Error loading the layer!")
         else:
+            # add layer to project
             project.addMapLayer(layer)
             project.write()
-            print(f"%{file} added to project\nProject saved successfully!")
+            print(f"{file} added to project\nProject saved successfully!")
