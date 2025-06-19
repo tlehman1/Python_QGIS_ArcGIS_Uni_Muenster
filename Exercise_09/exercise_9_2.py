294af1e8e7fc9ba24f4ff890f2683a2ce944efe3
TobiasBrand-GI <tobias.brand1@web.de>
2025-06-19 11:22:14 +0200

    test

--- Exercise_09/exercise_9_2.py
+++ Exercise_09/exercise_9_2.py
@@ -9,18 +9,18 @@ arcpy.management.AddField(assets, "buffer_distance", "TEXT")
 
 #function to calculate the buffer distance
 codeblock = """
-def buffer_distance(type):
+def calc_buffer_distance(type):
     if type == "mast":
-        return "300 meters"
+        return "300 Meters"
     if type == "mobile_antenna":
-        return "50 meters"
+        return "50 Meters"
     else:
-        return '100 meters'"""
+        return '100 Meters'"""
 
 #retrive the buffer distance depending on type
-expression = "buffer_distance(!type!)"
+expression = "calc_buffer_distance(!type!)"
 #calcualte Field
 arcpy.management.CalculateField(assets, "buffer_distance", expression,"PYTHON3", codeblock)
 
 #create buffer based on buffer_distance         
-arcpy.analysis.Buffer(assets, "coverage", "buffer_distance")
+arcpy.analysis.Buffer(assets,"coverage","buffer_distance")
